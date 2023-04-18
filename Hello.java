726544b7042244d27db64a5d05b73faa89daae49
ahleexx00 <130881431+ahleexx00@users.noreply.github.com>
2023-04-17 23:42:07 -0500

    Add java files

---
+++ Hello.java
@@ -0,0 +1,15 @@
+package sec03.exam01;
+
+/*
+ Author: Ahhyun Lee
+ Date: May 22th, 2022
+ */
+public class Hello {
+	//program entry point
+	public static void main(String[] args) {
+		//Statement printed
+		System.out.println("Hello, Java");
+
+	}//end of main
+
+}//end of class
